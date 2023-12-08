7efb43cf6677479cd416b9b01e8866f41b629a90
taquanglinh <tqlinh20194605@gmail.com>
2023-12-08 08:04:30 +0700

    FEAT: list of dishes, change dishes

--- src/main/java/dev/hikari/oishibasho/domain/restaurant/entity/Restaurant.java
+++ src/main/java/dev/hikari/oishibasho/domain/restaurant/entity/Restaurant.java
@@ -1,13 +1,17 @@
 package dev.hikari.oishibasho.domain.restaurant.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "restaurants")
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Restaurant {
 
     @Id
