c70548712138594aa9deba598b05a881d4c26512
taquanglinh <tqlinh20194605@gmail.com>
2023-12-17 20:19:51 +0700

    REFACTOR: ratings & special ratings

--- src/main/java/dev/hikari/oishibasho/domain/special_rating/service/SpecialRatingService.java
+++ src/main/java/dev/hikari/oishibasho/domain/special_rating/service/SpecialRatingService.java
@@ -1,11 +1,11 @@
 package dev.hikari.oishibasho.domain.special_rating.service;
 
+import dev.hikari.oishibasho.app.exception.ApiException;
 import dev.hikari.oishibasho.domain.food.entity.Food;
 import dev.hikari.oishibasho.domain.food.repository.FoodRepository;
 import dev.hikari.oishibasho.domain.special_rating.dto.response.SpecialRatingResponse;
 import dev.hikari.oishibasho.domain.special_rating.entity.SpecialRating;
 import dev.hikari.oishibasho.domain.special_rating.repository.SpecialRatingRepository;
-import dev.hikari.oishibasho.infrastructure.exception.ApiException;
 import dev.hikari.oishibasho.infrastructure.utilies.MessageCode;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +21,7 @@ public class SpecialRatingService {
 
     public SpecialRatingResponse getSpecialRating(Integer id) {
         Food food = foodRepository.findById(id).orElse(null);
-        if (food == null)throw new ApiException(MessageCode.FAIL);
+        if (food == null)throw new ApiException(MessageCode.ID_NOT_FOUND);
 
         SpecialRating specialRating = specialRatingRepository.findByFood(food).orElse(null);
         if (specialRating == null)throw new ApiException(MessageCode.FAIL);
@@ -29,6 +29,7 @@ public class SpecialRatingService {
                 .id(specialRating.getId())
                 .star(specialRating.getStar())
                 .video(specialRating.getVideo())
+                .review(specialRating.getReview())
                 .username(specialRating.getUser().getName())
                 .build();
     }
